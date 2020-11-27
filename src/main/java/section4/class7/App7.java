a231fc3c68e319a6f222c18c8fa08678d6d64302
gabriel7mendes <andrewsseven@gmail.com>
2020-11-27 17:55:26 -0300

    producer consumer with locks

---
+++ src/main/java/section4/class7/App7.java
@@ -0,0 +1,71 @@
+package section4.class7;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Worker {
+	
+	private Lock lock = new ReentrantLock();
+	private Condition condition = lock.newCondition();
+	
+	public void producer() throws InterruptedException {
+		lock.lock();
+		System.out.println("Producer method...");
+		condition.await();
+		System.out.println("Producer again...");
+		lock.unlock();
+	}
+	
+    public void consumer() throws InterruptedException {
+    	
+    	lock.lock();
+		Thread.sleep(2000);
+		System.out.println("Consumer method...");
+		condition.signal();
+		lock.unlock();
+	}
+}
+
+public class App7 {
+	
+	public static void main(String[] args) {
+		
+		Worker worker = new Worker();
+		
+		Thread t1 = new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					try {
+						worker.producer();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+			
+	        Thread t2 = new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					try {
+						worker.consumer();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+	        
+	        t1.start();
+	        t2.start();
+	        
+	        try {
+				t1.join();
+			    t2.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+	}
+
+}

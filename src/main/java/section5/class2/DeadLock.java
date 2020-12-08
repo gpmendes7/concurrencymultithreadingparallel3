c3b9d02506ede1b8421d5594def5fe9bfa3618f3
gabriel7mendes <andrewsseven@gmail.com>
2020-12-08 17:49:51 -0300

    deadlock and livelock

---
+++ src/main/java/section5/class2/DeadLock.java
@@ -0,0 +1,52 @@
+package section5.class2;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class DeadLock {
+	
+	private Lock lock1 = new ReentrantLock(true);
+	private Lock lock2 = new ReentrantLock(true);
+	
+	public static void main(String[] args) {
+		DeadLock deadlock = new DeadLock();
+		
+		new Thread(deadlock::worker1, "worker1").start();
+		new Thread(deadlock::worker2, "worker2").start();
+	}
+	
+	public void worker1() {
+		lock1.lock();
+		System.out.println("Worker1 acquires the lock1...");
+		
+		try {
+			Thread.sleep(300);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		lock2.lock();
+		System.out.println("Worker1 acquired the lock2...");
+		
+		lock1.unlock();
+		lock2.unlock();
+	}
+	
+	public void worker2() {
+		lock1.lock();
+		System.out.println("Worker2 acquires the lock1...");
+		
+		try {
+			Thread.sleep(300);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		lock2.lock();
+		System.out.println("Worker2 acquired the lock2...");
+		
+		lock1.unlock();
+		lock2.unlock();
+	}
+
+}
---
+++ src/main/java/section5/class3/LiveLock.java
@@ -0,0 +1,71 @@
+package section5.class3;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LiveLock {
+	
+	private Lock lock1 = new ReentrantLock(true);
+	private Lock lock2 = new ReentrantLock(true);
+	
+	public static void main(String[] args) {
+		LiveLock livelock = new LiveLock();
+		
+		new Thread(livelock::worker1, "worker1").start();
+		new Thread(livelock::worker2, "worker2").start();
+	}
+	
+	public void worker1() {
+		while(true) {
+			try {
+				lock1.tryLock(50, TimeUnit.MILLISECONDS);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			System.out.println("Worker1 acquires the lock1...");
+			System.out.println("Worker1 tries to get lock2...");
+			
+			if(lock2.tryLock()) {
+			   System.out.println("Worker1 acquires the lock2...");
+			   lock2.unlock();
+			} else {
+				System.out.println("Worker1 can not acquire the lock2...");
+				continue;
+			}
+			
+			break;
+		}
+		
+		lock1.unlock();
+		lock2.unlock();
+	}
+	
+	public void worker2() {
+		while(true) {
+			try {
+				lock2.tryLock(50, TimeUnit.MILLISECONDS);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			
+			System.out.println("Worker2 acquires the lock2...");
+			System.out.println("Worker2 tries to get lock1...");
+			
+			if(lock1.tryLock()) {
+			    System.out.println("Worker2 acquires the lock1...");
+				lock1.unlock();
+			} else {
+				System.out.println("Worker2 can not acquire the lock1...");
+				continue;
+			}
+			
+			break;
+		}
+		
+		lock1.unlock();
+		lock2.unlock();
+	}
+	
+}

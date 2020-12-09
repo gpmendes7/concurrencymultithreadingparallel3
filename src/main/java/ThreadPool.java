ef22648f237458955b8096285a2d9f8b78f19a5b
gabriel7mendes <andrewsseven@gmail.com>
2020-12-09 14:11:29 -0300

    fixedthreadpools

--- src/main/java/ThreadPool.java
+++ src/main/java/ThreadPool.java
@@ -1,17 +1,18 @@
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-class Task implements Runnable {
+class Work implements Runnable {
 	private int id;
 	
-	public Task(int id) {
+	public Work(int id) {
 		this.id = id;
 	}
 	
 	@Override
 	public void run() {
-		System.out.println("Task with id " + id + " is in work - thread id: " + Thread.currentThread().getName());
+		System.out.println("Task with id " + id + " is in work - thread id: " + Thread.currentThread().getId());
 		long duration = (long) Math.random()*5;
 		try {
 			TimeUnit.SECONDS.sleep(duration);
@@ -21,13 +22,13 @@ class Task implements Runnable {
 	}
 }
 
-public class SingleThreadExecutor {
+public class ThreadPool {
 	
 	public static void main(String[] args) {	
-		ExecutorService executor = Executors.newSingleThreadExecutor();
+		ExecutorService executor = Executors.newFixedThreadPool(10);
 		
-		for(int i=0; i<5;i++)
-			executor.execute(new Task(i));
+		for(int i=0; i<100;i++)
+			executor.execute(new Work(i+1));
 	}
 
 }
